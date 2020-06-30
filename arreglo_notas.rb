12152a2e6b353bc14834768bb867ec8bbd621a0f
Andrés <andcarv89@gmail.com>
2020-06-30 14:51:35 -0400

    minor change in variable name

--- arreglo_notas.rb
+++ arreglo_notas.rb
@@ -2,15 +2,17 @@ mark = [5,7,1,3,5,8,9,'N.A','N.A',3]
 
 def promedio (mark)
     mean = 0
-    count = mark.count
-    count.times do |i|
+    numb = mark.count
+    numb.times do |i|
         mark[i] = 2 if mark[i] == 'N.A'
     end
     mark.each do |mark|
-        mean += (mark/count.to_f)
+        mean += (mark/numb.to_f)
     end 
     print mean
     print "\n"   
 end
 
 promedio(mark)
+
+# se puede haber hecho con collect el cambio a string, mark.collect {|i| i.to_f}
